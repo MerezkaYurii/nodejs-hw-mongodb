b6e217894b66c840420ecdf1e87ab79d68d4e8db
MerezkaYurii <shkolasad@ukr.net>
2025-03-28 08:26:21 +0200

    Initial commit

---
+++ src/scripts/generateContacts.js
@@ -0,0 +1,13 @@
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const contactsList = await readContacts();
+  const newContacts = Array(number)
+    .fill(0)
+    .map((item) => createFakeContact(item));
+  return writeContacts([...contactsList, ...newContacts]);
+};
+
+generateContacts(5);
---
+++ src/scripts/removeLastContact.js
@@ -0,0 +1,9 @@
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+export const removeLastContact = async () => {
+  const contactsList = await readContacts();
+  contactsList.pop();
+  await writeContacts(contactsList);
+};
+
+removeLastContact();
