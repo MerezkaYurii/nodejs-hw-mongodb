67963033bb625e09914d52f025a1e52dd00276d4
MerezkaYurii <shkolasad@ukr.net>
2025-05-02 09:21:32 +0300

    hw-7-var1

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import authRouter from './routers/auth.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 export const setupServer = () => {
   const app = express();
@@ -17,10 +18,12 @@ export const setupServer = () => {
 
   app.use(express.json());
 
-  app.use(logger);
+  // app.use(logger);
   app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
   app.use('/uploads', express.static(UPLOAD_DIR));
+
+  app.use('/api-docs', swaggerDocs());
   app.use(notFoundHandler);
 
   app.use(errorHandler);
